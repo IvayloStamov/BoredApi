54c0daabf1d7e0db3318eb9604fa4a9bb9316d4b
Ivaylo Stamov <ivaylo.stamov@mentormate.com>
2022-07-14 09:35:57 +0300

    Add "Config" and "Data" classes to the "Data" library

---
+++ Data/Data.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+        {
+
+        }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Activity> Activities { get; set; }
+        public DbSet<UserActivity> UserActivities { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserActivity>().HasKey(ua => new
+            {
+                ua.ActivityId,
+                ua.UserId
+            });
+
+            modelBuilder.Entity<UserActivity>()
+                .HasOne(ua => ua.User)
+                .WithMany(ua => ua.UserActivities)
+                .HasForeignKey(ua => ua.UserId);
+
+            modelBuilder.Entity<UserActivity>()
+                .HasOne(ua => ua.Activity)
+                .WithMany(ua => ua.UserActivities)
+                .HasForeignKey(ua => ua.ActivityId);
+        }
+    }
+}
