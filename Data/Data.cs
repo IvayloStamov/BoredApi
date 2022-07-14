4b2083ca106d6b1cf930cfa608c951b8c10ba962
Ivaylo Stamov <ivaylo.stamov@mentormate.com>
2022-07-14 09:58:43 +0300

    Seed data into the "Activities" table

--- Data/Data.cs
+++ Data/Data.cs
@@ -36,6 +36,23 @@ namespace Data
                 .HasOne(ua => ua.Activity)
                 .WithMany(ua => ua.UserActivities)
                 .HasForeignKey(ua => ua.ActivityId);
+
+
+            //DataSeeding
+            
+            //modelBuilder.Entity<Activity>()
+            //    .HasData(
+            //    new Activity { ActivityId = 1, ActivityName = "education" },
+            //    new Activity { ActivityId = 2, ActivityName = "recreational" },
+            //    new Activity { ActivityId = 3, ActivityName = "social" },
+            //    new Activity { ActivityId = 4, ActivityName = "diy" },
+            //    new Activity { ActivityId = 5, ActivityName = "charity" },
+            //    new Activity { ActivityId = 6, ActivityName = "cooking" },
+            //    new Activity { ActivityId = 7, ActivityName = "relaxation" },
+            //    new Activity { ActivityId = 8, ActivityName = "music" },
+            //    new Activity { ActivityId = 9, ActivityName = "busywork" }
+            //    );
+                
         }
     }
 }
--- Data/Migrations/20220714065624_SeedActivityData.Designer.cs
+++ Data/Migrations/20220714065624_SeedActivityData.Designer.cs
@@ -3,6 +3,7 @@ using Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20220714065624_SeedActivityData")]
+    partial class SeedActivityData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
