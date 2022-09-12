feabddb3658806eb06124a5167bd523aad7d3bbf
Ivaylo Stamov <ivaylo.stamov@mentormate.com>
2022-09-12 23:03:52 +0300

    Commit tests

---
+++ BoredApi.Test/Mocks/UserServiceMock.cs
@@ -0,0 +1,31 @@
+﻿using BoredApi.Services;
+using Models;
+using Moq;
+using Microsoft.AspNetCore.Mvc;
+namespace BoredApi.Test.Mocks
+{
+    public static class UserServiceMock
+    {
+        public static IUserService Instance
+        {
+            get
+            {
+                var userServiceMock = new Mock<IUserService>();
+                var users = new List<UserDto>()
+                {
+                    new UserDto()
+                    {
+                        FirstName = "1",
+                        LastName = "1",
+                        Username = "1"
+                    }
+                };
+                userServiceMock
+                    .Setup(x => x.GetAllUsersAsync())
+                    .Returns(Task.FromResult(users));
+
+                return userServiceMock.Object;
+            }
+        }
+    }
+}
---
+++ BoredApi.Test/Services/RequestServiceTest.cs
@@ -0,0 +1,84 @@
+﻿using BoredApi.Data.DataModels.Enums;
+using BoredApi.Data.Models;
+using BoredApi.Services;
+using BoredApi.Services.ViewModels;
+using BoredApi.Test.Mocks;
+using Xunit;
+
+namespace BoredApi.Test.Services
+{
+    public class RequestServiceTest
+    {
+        public RequestServiceTest()
+        {
+
+        }
+        [Fact]
+        public async Task ChangeRequestStatusAsync_ShouldChangeTheStatusOfTheRequest1()
+        {
+            //Arrange
+            using var data = DatabaseMock.Instance;
+            var userOne = new User { Id = 1 };
+            var userTwo = new User { Id = 2 };
+
+            var groupOne = new Group { Id = 1, OwnerId = 1 };
+            var groupActiviryOne = new GroupActivity { Id = 1 ,
+                GroupId = 1, ActivityId = 1, Status = Status.Pending, StartDate = DateTime.Now};
+            var joinRequestOne = new JoinActivityRequest { UserId = 1, GroupActivityId = 1, HasAccepted = Status.Pending };
+            data.Users.Add(userOne);
+            data.Users.Add(userTwo);
+            data.Groups.Add(groupOne);
+            
+            data.GroupActivities.Add(groupActiviryOne);
+            await data.SaveChangesAsync();
+            data.JoinActivityRequests.Add(joinRequestOne);
+
+            await data.SaveChangesAsync();
+            var groupService = new GroupService(data);
+            await groupService.AddUserToGroupAsync(1, 2, 1);
+            var requestService = new RequestService(data);
+            var changeRequestDto = new ChangeRequestStatusDto { Status = Status.Accepted };
+
+            //Act
+
+            var result = await requestService.ChangeRequestStatusAsync(2, 1, changeRequestDto);
+
+            //Assert
+
+            Assert.Equal(Status.Accepted, result.Value.HasAccepted);
+        }
+        [Fact]
+        public async Task ChangeRequestStatusAsync_ShouldChangeTheStatusOfTheRequest()
+        {
+            //Arrange
+            using var data = DatabaseMock.Instance;
+            var userOne = new User { Id = 1 };
+            var userTwo = new User { Id = 2 };
+
+            var groupOne = new Group { Id = 1, OwnerId = 1 };
+           
+            var joinRequestOne = new JoinActivityRequest { UserId = 1, GroupActivityId = 1, HasAccepted = Status.Pending };
+            data.Users.Add(userOne);
+            data.Users.Add(userTwo);
+            data.Groups.Add(groupOne);
+
+            data.GroupActivities.Add(groupActiviryOne);
+            await data.SaveChangesAsync();
+            data.JoinActivityRequests.Add(joinRequestOne);
+
+            await data.SaveChangesAsync();
+            var groupService = new GroupService(data);
+            await groupService.AddUserToGroupAsync(1, 2, 1);
+            var requestService = new RequestService(data);
+            var changeRequestDto = new ChangeRequestStatusDto { Status = Status.Accepted };
+
+            //Act
+
+            var result = await requestService.ChangeRequestStatusAsync(2, 1, changeRequestDto);
+
+            //Assert
+
+            Assert.Equal(Status.Accepted, result.Value.HasAccepted);
+        }
+    }
+}
